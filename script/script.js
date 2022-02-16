bf07f8ea4c78050c46e7377b20c94405bfe29d01
Faisal Karim <faisalchy64@gmail.com>
2022-02-17 05:25:37 +0600

    select dom elements

---
+++ script/script.js
@@ -0,0 +1,13 @@
+// select elements
+const incomeInput = document.getElementById("income-input");
+const foodInput = document.getElementById("food-input");
+const rentInput = document.getElementById("rent-input");
+const clothInput = document.getElementById("cloth-input");
+const saveInput = document.getElementById("save-input");
+const totalExpenses = document.getElementById("total-expenses");
+const balance = document.getElementById("balance");
+const savingAmount = document.getElementById("saving-amount");
+const remainingBalance = document.getElementById("remaining-balance");
+const errorMessage = document.getElementById("error-message");
+
+
